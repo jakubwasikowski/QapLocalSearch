b8a9dd3ac9f61c16f8955a666cfdd2a33c498a07
Kuba Wąsikowski <jakub.wasikowski@gmail.com>
2014-10-18 21:51:55 +0200

    Changed states for problem parser

--- src/edu/mioib/qaplocalsearch/parser/ProblemParser.java
+++ src/edu/mioib/qaplocalsearch/parser/ProblemParser.java
@@ -3,35 +3,40 @@ package edu.mioib.qaplocalsearch.parser;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+
 import edu.mioib.qaplocalsearch.model.Problem;
 
 public class ProblemParser {
+	private enum ParseState {
+		PROBLEM_SIZE, LOCALIZATIONS, FACILITIES
+	}
+
 	public static Problem parseProblemFile(String path) {
 		
 		Problem result = null;
 		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
 			String line = null;
-			int problemState = 0;
+			ParseState state = ParseState.PROBLEM_SIZE;
 			int problemSize = 0;
 			int[][] localizationMatrix = null;
 			int[][] facilitiesMatrix = null;
 			int matrixCounter = 0;
 			while ((line = bufferedReader.readLine()) != null) {
-				if (problemState == 0) {
+				if (state == ParseState.PROBLEM_SIZE) {
 					problemSize = Integer.parseInt(line);
 					localizationMatrix = new int[problemSize][problemSize];
 					facilitiesMatrix = new int[problemSize][problemSize];
-					problemState++;
-				} else if (problemState==1 && !line.trim().isEmpty()) {
+					state = ParseState.LOCALIZATIONS;
+				} else if (state == ParseState.LOCALIZATIONS && !line.trim().isEmpty()) {
 					String[] matrixElems = line.trim().split(" ");
 					for(int i=0; i<matrixElems.length; i++){
 						localizationMatrix[matrixCounter][i] = Integer.parseInt(matrixElems[i]);
 					}
 					matrixCounter++;
-				} else if (problemState==1 && line.trim().isEmpty()){
-					problemState++;
+				} else if (state == ParseState.LOCALIZATIONS && line.trim().isEmpty()) {
 					matrixCounter = 0;
-				} else if(problemState==2 && !line.trim().isEmpty()){
+					state = ParseState.FACILITIES;
+				} else if (state == ParseState.FACILITIES && !line.trim().isEmpty()) {
 					String[] matrixElems = line.trim().split(" ");
 					for(int i=0; i<matrixElems.length; i++){
 						facilitiesMatrix[matrixCounter][i] = Integer.parseInt(matrixElems[i]);
