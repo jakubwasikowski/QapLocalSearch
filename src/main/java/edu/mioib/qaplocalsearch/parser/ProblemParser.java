9b3ffaf09862a63831064d00ef1a3f1fbdef8563
Kuba Wąsikowski <jakub.wasikowski@gmail.com>
2014-10-19 22:25:50 +0200

    Changed QapEvaluatorUnitTest

--- src/main/java/edu/mioib/qaplocalsearch/parser/ProblemParser.java
+++ src/main/java/edu/mioib/qaplocalsearch/parser/ProblemParser.java
@@ -7,19 +7,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.ParseException;
+
 import edu.mioib.qaplocalsearch.model.Problem;
 
 public class ProblemParser {
 	private enum ParseState {
-		PROBLEM_SIZE, LOCALIZATIONS, FACILITIES
+		PROBLEM_SIZE, PROBLEM_SIZE_SPACE, LOCALIZATIONS, LOCALIZATIONS_SPACE, FACILITIES, END
 	}
 	
 	public static Problem parseProblemFile(String path) throws NumberFormatException, FileNotFoundException,
-			IOException {
+			IOException, ParseException {
 		return parseProblemFile(new FileInputStream(path));
 	}
 
-	public static Problem parseProblemFile(InputStream inputStream) throws NumberFormatException, IOException {
+	public static Problem parseProblemFile(InputStream inputStream) throws NumberFormatException, IOException,
+			ParseException {
 		Problem result = null;
 		try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
 			String line = null;
@@ -29,26 +32,41 @@ public class ProblemParser {
 			int[][] facilitiesMatrix = null;
 			int matrixCounter = 0;
 			while ((line = bufferedReader.readLine()) != null) {
+				line = line.trim();
 				if (state == ParseState.PROBLEM_SIZE) {
-					problemSize = Integer.parseInt(line.trim());
+					problemSize = Integer.parseInt(line);
 					localizationMatrix = new int[problemSize][problemSize];
 					facilitiesMatrix = new int[problemSize][problemSize];
+					state = ParseState.PROBLEM_SIZE_SPACE;
+				} else if (state == ParseState.PROBLEM_SIZE_SPACE) {
+					if (!line.isEmpty()) {
+						throw new ParseException();
+					}
 					state = ParseState.LOCALIZATIONS;
-				} else if (state == ParseState.LOCALIZATIONS && !line.trim().isEmpty()) {
-					String[] matrixElems = line.trim().split("\\s+");
+				} else if (state == ParseState.LOCALIZATIONS) {
+					String[] matrixElems = line.split("\\s+");
 					for(int i=0; i<matrixElems.length; i++){
 						localizationMatrix[matrixCounter][i] = Integer.parseInt(matrixElems[i]);
 					}
 					matrixCounter++;
-				} else if (state == ParseState.LOCALIZATIONS && line.trim().isEmpty()) {
+					if (matrixCounter == problemSize) {
+						state = ParseState.LOCALIZATIONS_SPACE;
+					}
+				} else if (state == ParseState.LOCALIZATIONS_SPACE) {
+					if (!line.isEmpty()) {
+						throw new ParseException();
+					}
 					matrixCounter = 0;
 					state = ParseState.FACILITIES;
-				} else if (state == ParseState.FACILITIES && !line.trim().isEmpty()) {
-					String[] matrixElems = line.trim().split("\\s+");
+				} else if (state == ParseState.FACILITIES) {
+					String[] matrixElems = line.split("\\s+");
 					for (int i = 0; i < matrixElems.length; i++) {
 						facilitiesMatrix[matrixCounter][i] = Integer.parseInt(matrixElems[i]);
 					}
 					matrixCounter++;
+					if (matrixCounter == problemSize) {
+						state = ParseState.END;
+					}
 				}
 			}
 			result = new Problem(problemSize, localizationMatrix, facilitiesMatrix);
