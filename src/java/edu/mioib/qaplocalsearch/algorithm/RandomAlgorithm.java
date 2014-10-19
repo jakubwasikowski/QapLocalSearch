f56cccf44250fd667e6aebf84c85c9605eb15c46
kaathleen <katarzyna.zjawin@student.put.poznan.pl>
2014-10-19 19:07:50 +0200

    Random Algorithm

--- src/java/edu/mioib/qaplocalsearch/algorithm/RandomAlgorithm.java
+++ src/java/edu/mioib/qaplocalsearch/algorithm/RandomAlgorithm.java
@@ -1,5 +1,7 @@
 package edu.mioib.qaplocalsearch.algorithm;
 
+import java.util.Random;
+
 import edu.mioib.qaplocalsearch.algorithm.neighboursgenerator.TwoOptNeighboursIterator;
 import edu.mioib.qaplocalsearch.model.Problem;
 import edu.mioib.qaplocalsearch.model.Solution;
@@ -15,16 +17,27 @@ public class RandomAlgorithm implements Algorithm {
 		TwoOptNeighboursIterator neighbourIterator;
 		do{
 			neighbourIterator = new TwoOptNeighboursIterator(currentState);
-			currentStateChanged = false;
-			while(neighbourIterator.hasNext() && !currentStateChanged){
-				int[] newState = neighbourIterator.next();
+			int neighboursNumber = neighbourIterator.getNeighboursNumber();
+			int[][] currentNighboursArray = new int[neighboursNumber][currentState.length];
+			int counter=0;
+			while(neighbourIterator.hasNext()){
+				currentNighboursArray[counter]=neighbourIterator.next();
+				counter++;
+			}
+			
+			Random rand = new Random();
+			
+			for(int i=0; i<neighboursNumber; i++){
+				int randomNum = rand.nextInt(neighboursNumber-i);
+				int[] newState = currentNighboursArray[randomNum];
 				int newStateEvaluation = evaluateState(problem,newState);
 				if(newStateEvaluation > currentEvaluation){
 					currentState = newState;
 					currentEvaluation = newStateEvaluation;
 					currentStateChanged = true;
+					break;
 				}
-			}
+			}			
 		} while(currentStateChanged);
 		
 		return new Solution(currentEvaluation, currentState);
